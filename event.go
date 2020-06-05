328798e0ab4d07a95a57f3d5d20022ba0e9b9f27
maruware <me@maruware.com>
2020-06-05 16:22:52 +0900

    fix mistake def

--- event.go
+++ event.go
@@ -10,36 +10,36 @@ type Event struct {
 	event *C.GstEvent
 }
 
-const (
-	EventUnknown                MessageType = C.GST_EVENT_UNKNOWN
-	EventFlushStart             MessageType = C.GST_EVENT_FLUSH_START
-	EventFlushStop              MessageType = C.GST_EVENT_FLUSH_STOP
-	EventStreamStart            MessageType = C.GST_EVENT_STREAM_START
-	EventCaps                   MessageType = C.GST_EVENT_CAPS
-	EventSegment                MessageType = C.GST_EVENT_SEGMENT
-	EventTag                    MessageType = C.GST_EVENT_TAG
-	EventBuffersize             MessageType = C.GST_EVENT_BUFFERSIZE
-	EventSinkMessage            MessageType = C.GST_EVENT_SINK_MESSAGE
-	EventEos                    MessageType = C.GST_EVENT_EOS
-	EventToc                    MessageType = C.GST_EVENT_TOC
-	EventSegmentDone            MessageType = C.GST_EVENT_SEGMENT_DONE
-	EventGap                    MessageType = C.GST_EVENT_GAP
-	EventQos                    MessageType = C.GST_EVENT_QOS
-	EventSeek                   MessageType = C.GST_EVENT_SEEK
-	EventNavigation             MessageType = C.GST_EVENT_NAVIGATION
-	EventLatency                MessageType = C.GST_EVENT_LATENCY
-	EventStep                   MessageType = C.GST_EVENT_STEP
-	EventReconfigure            MessageType = C.GST_EVENT_RECONFIGURE
-	EventTocSelect              MessageType = C.GST_EVENT_TOC_SELECT
-	EventCustomUpstream         MessageType = C.GST_EVENT_CUSTOM_UPSTREAM
-	EventCustomDownstream       MessageType = C.GST_EVENT_CUSTOM_DOWNSTREAM
-	EventCustomDownstreamOob    MessageType = C.GST_EVENT_CUSTOM_DOWNSTREAM_OOB
-	EventCustomDownstreamSticky MessageType = C.GST_EVENT_CUSTOM_DOWNSTREAM_STICKY
-	EventCustomBoth             MessageType = C.GST_EVENT_CUSTOM_BOTH
-	EventCustomBothOob          MessageType = C.GST_EVENT_CUSTOM_BOTH_OOB
-)
-
 type EventType C.GstEventType
+
+const (
+	EventUnknown                EventType = C.GST_EVENT_UNKNOWN
+	EventFlushStart             EventType = C.GST_EVENT_FLUSH_START
+	EventFlushStop              EventType = C.GST_EVENT_FLUSH_STOP
+	EventStreamStart            EventType = C.GST_EVENT_STREAM_START
+	EventCaps                   EventType = C.GST_EVENT_CAPS
+	EventSegment                EventType = C.GST_EVENT_SEGMENT
+	EventTag                    EventType = C.GST_EVENT_TAG
+	EventBuffersize             EventType = C.GST_EVENT_BUFFERSIZE
+	EventSinkMessage            EventType = C.GST_EVENT_SINK_MESSAGE
+	EventEos                    EventType = C.GST_EVENT_EOS
+	EventToc                    EventType = C.GST_EVENT_TOC
+	EventSegmentDone            EventType = C.GST_EVENT_SEGMENT_DONE
+	EventGap                    EventType = C.GST_EVENT_GAP
+	EventQos                    EventType = C.GST_EVENT_QOS
+	EventSeek                   EventType = C.GST_EVENT_SEEK
+	EventNavigation             EventType = C.GST_EVENT_NAVIGATION
+	EventLatency                EventType = C.GST_EVENT_LATENCY
+	EventStep                   EventType = C.GST_EVENT_STEP
+	EventReconfigure            EventType = C.GST_EVENT_RECONFIGURE
+	EventTocSelect              EventType = C.GST_EVENT_TOC_SELECT
+	EventCustomUpstream         EventType = C.GST_EVENT_CUSTOM_UPSTREAM
+	EventCustomDownstream       EventType = C.GST_EVENT_CUSTOM_DOWNSTREAM
+	EventCustomDownstreamOob    EventType = C.GST_EVENT_CUSTOM_DOWNSTREAM_OOB
+	EventCustomDownstreamSticky EventType = C.GST_EVENT_CUSTOM_DOWNSTREAM_STICKY
+	EventCustomBoth             EventType = C.GST_EVENT_CUSTOM_BOTH
+	EventCustomBothOob          EventType = C.GST_EVENT_CUSTOM_BOTH_OOB
+)
 
 func (e *Event) GetType() EventType {
 	ctype := C.X_GST_EVENT_TYPE(e.event)
